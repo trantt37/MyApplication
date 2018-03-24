b3f4077ab5bbd382de4bf0b807894311e7d10d11
Timmy Tran <trantt37@vcu.edu>
2018-03-24 11:03:58 -0400

    Created new tests to run.

--- app/src/androidTest/java/com/example/carolinebudwell/myapplication/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/carolinebudwell/myapplication/ExampleInstrumentedTest.java
@@ -2,11 +2,18 @@ package com.example.carolinebudwell.myapplication;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.*;
 
 /**
@@ -22,5 +29,23 @@ public class ExampleInstrumentedTest {
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("com.example.carolinebudwell.myapplication", appContext.getPackageName());
+    }
+    @Rule
+    public ActivityTestRule<MainActivity> mButtonTestRule =
+            new ActivityTestRule<MainActivity>(MainActivity.class);
+    @Test
+    public void MainActivityButtonSwitchToEnterValuesActivity() throws Exception{
+        onView(withId(R.id.button))
+                .perform(click());
+        onView(withId(R.id.button3))
+                .check(matches(isDisplayed()));
+        onView(withId(R.id.editText))
+                .check(matches(isDisplayed()));
+        onView(withId(R.id.editText2))
+                .check(matches(isDisplayed()));
+        onView(withId(R.id.textView3))
+                .check(matches(isDisplayed()));
+        onView(withId(R.id.textView))
+                .check(matches(isDisplayed()));
     }
 }
